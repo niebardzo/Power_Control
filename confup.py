41e45b03efa7cf7f4d9cd21adf1dbcde2a6f460f
Aleksandra Sucharska (UST <U58222@ust-global.com>
2017-07-20 15:34:08 +0200

    edit: spacje

--- confup.py
+++ confup.py
@@ -1,23 +1,22 @@
 def confRead():
     with open('conf.cfg', 'r') as f:
-        conf={'target': 75,
-              'hister': 3,
-              'maxInc': 8,
-              'maxIncHist': 1,
-              'maxDec': 4,
-              'maxDecHist': 1,
-              'changeThresh': 1,
-              'maxMissing': 3,
-              'window': 8}
+        conf = {'target': 75,
+                'hister': 3,
+                'maxInc': 8,
+                'maxIncHist': 1,
+                'maxDec': 4,
+                'maxDecHist': 1,
+                'changeThresh': 1,
+                'maxMissing': 3,
+                'window': 8}
         while True:
             try:
                 confLine = f.readline().replace('\n', '')
                 if confLine.startswith('#'):
                     pass
                 else:
-                    conf[confLine.split(':')[0]]= int(confLine.split(':')[1])
+                    conf[confLine.split(':')[0]] = int(confLine.split(':')[1])
             except:
                 break
-
 
     return conf
