3723cda71acf13f2e90508d96558c92f9c560282
Patryk Bogusz (UST <U58223@ust-global.com>
2017-07-21 16:43:58 +0200

    Fix handover

--- handover.py
+++ handover.py
@@ -12,17 +12,17 @@ def handover_a(f_line, avg_power, hister, target):
     :param target: power target
     :return: string
     """
-    cell = f_line[0]
+    cell = f_line[1]
     if cell == 'S0':
         return 1
     else:
         dist_s0 = fabs(avg_power) + fabs(hister) - fabs(target)
-        dist_nx = fabs(target) - fabs(f_line[2])
+        dist_nx = fabs(target) - fabs(int(f_line[3]))
         if fabs(dist_nx) < fabs(dist_s0):
             return 3
         else:
             return 2
 
 
-print(handover_a(['NO', 'MAsdsa', -60, 3], -50, 7, -58))
-print(handover_a(['NO', 'MAsdsa', -60, 3], -50, 7, -58))
+print(handover_a(['DL' , 'NO', 'MAsdsa', -60, 3], -50, 7, -58))
+print(handover_a(['DL','S0', 'MAsdsa', '-60', 3], -50, 7, -58))
