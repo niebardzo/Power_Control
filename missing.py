187ab3dc1cb4540fd39f9c3eb0bfe0b06a1baafb
Patryk Bogusz (UST <U58223@ust-global.com>
2017-07-21 13:45:44 +0200

    Fix missing.py with tests

--- missing.py
+++ missing.py
@@ -11,14 +11,26 @@ def missing_power(unresolved, m, count):
     resolved = [0 for _ in range(len(unresolved))]
     if unresolved[0] == "missing" or unresolved[0] == "":
         resolved[0] = "-95"
-    for i in range(len(unresolved)):
-        if unresolved[i] == "missing" or unresolved[i] == "":
-            count += 1
-            if 1 <= count <= m:
-                resolved[i] = int(unresolved[i-1])
-            if count > m:
-                resolved[i] = -95
-        else:
+    if unresolved[len(unresolved)-1] == "missing" or unresolved[len(unresolved)-1] == "":
+        if 1 <= count <= m:
+            resolved[len(resolved)-1] = int(unresolved[len(resolved)-2])
+        if count > m:
+            resolved[len(resolved)-1] = int(-95)
+        count += 1
+    else:
+        resolved[len(resolved) - 1] = int(unresolved[len(resolved)-1])
+        count = 0
+
+    for i in range(len(unresolved)-1):
             resolved[i] = int(unresolved[i])
-            count = 0
     return resolved, count
+
+
+print('testing', missing_power([-50, -60, -80, 'missing'], 2, 2))
+print("Expected result ([-50,-60,-80,-95],3)", '\n')
+print('testing', missing_power([-50, -60, -80, -50], 2, 5))
+print("Expected result ([-50,-60,-80,-50],0)", '\n')
+print('testing', missing_power([-50, -60, -80, 'missing'], 2, 1))
+print("Expected result ([-50,-60,-80,-80],2)", '\n')
+print('testing', missing_power([-50, -60, -80, -50], 2, 2))
+print("Expected result ([-50,-60,-80,-50],0)", '\n')
