4c4d49eef9c7ef102d62f8874c02bf5c8a779e96
Aleksandra Sucharska (UST <U58222@ust-global.com>
2017-07-21 16:11:29 +0200

    Removed unnecesary print line

--- read_file.py
+++ read_file.py
@@ -12,7 +12,7 @@ def read_file():
     input_data = sys.stdin.readline()
 
     if input_data:
-        print(input_data, parameter_list)
+        #print(input_data, parameter_list)
         parameter = input_data.split()
 
         for _ in range(len(parameter)):
