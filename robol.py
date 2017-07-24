dc3cc1a828cc3bce78b718b189fdef2b49851b0e
Szymon Bieniek (UST <U58225@ust-global.com>
2017-07-24 20:03:42 +0200

    updating robol

--- robol.py
+++ robol.py
@@ -19,44 +19,41 @@ def worker(data, conf):
     quality = data[1]
     x = conf['target'] - signal
     if quality < 2.0:
-        if signal > (conf['target'] + conf['hister']):
+        if signal >= (conf['target'] + conf['hister']):
+            action = 'DEC'
             if math.fabs(x) >= conf['maxDec']:
-                action = 'DEC'
                 value = conf['maxDec']
             else:
-                action = 'DEC'
                 value = math.fabs(x)
-        elif signal < (conf['target'] - conf['hister']):
+        elif signal <= (conf['target'] - conf['hister']):
+            action = 'INC'
             if x >= conf['maxInc']:
-                action = 'INC'
                 value = conf['maxInc']
             else:
-                action = 'INC'
                 value = x
-        elif (conf['target'] - conf['changeThresh']) <= signal <= (conf['target'] + conf['changeThresh']):
+        elif (conf['target'] - conf['changeThresh']) < signal < (conf['target'] + conf['changeThresh']):
             action = 'NCH'
             value = ''
-        elif (conf['target'] - conf['hister']) <= signal < (conf['target'] - conf['changeThresh']):
+        elif (conf['target'] - conf['hister']) < signal <= (conf['target'] - conf['changeThresh']):
             action = 'INC'
             value = conf['maxIncHist']
-        elif (conf['target'] + conf['changeThresh']) < signal <= (conf['target'] + conf['hister']):
-            action = 'INC'
-            value = conf['maxIncHist']
+        elif (conf['target'] + conf['changeThresh']) <= signal < (conf['target'] + conf['hister']):
+            action = 'DEC'
+            value = conf['maxDecHist']
     elif 2.0 <= quality < 4:
-        if (conf['target'] - conf['changeThresh']) <= signal <= (conf['target'] + conf['changeThresh']):
+        if (conf['target'] - conf['changeThresh']) < signal < (conf['target'] + conf['changeThresh']):
             action = 'NCH'
             value = ''
         elif signal <= (conf['target'] - conf['hister']):
+            action = 'INC'
             if x >= conf['maxInc']:
-                action = 'INC'
                 value = conf['maxInc']
             else:
-                action = 'INC'
                 value = x
-        elif (conf['target'] - conf['hister']) < signal < (conf['target'] - conf['changeThresh']):
+        elif (conf['target'] - conf['hister']) < signal <= (conf['target'] - conf['changeThresh']):
             action = 'INC'
             value = conf['maxIncHist']
-        elif signal > (conf['target'] + conf['changeThresh']):
+        elif signal >= (conf['target'] + conf['changeThresh']):
             action = 'NCH'
             value = ''
     elif quality >= 4:
