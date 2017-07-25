3c07470ed47a2668eb7692a26138396ab4b2cefe
Patryk Bogusz (UST <U58223@ust-global.com>
2017-07-25 13:29:05 +0200

    Docstring to HTTP

--- http_request.py
+++ http_request.py
@@ -2,6 +2,11 @@ import requests
 
 
 def request(line):
+    """
+    Function using requests library to send data(line) via get
+    to local server on port 8000.
+    :param line: data to be send
+    """
     buffer = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
     for i in range(5):
         buffer[i + 1] = line[i]
--- http_server.py
+++ http_server.py
@@ -5,6 +5,11 @@ import urllib.parse
 
 class SimpleHTTPRequestHandler1(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        """
+        Overwriting class method to send data to database through
+        get request. After receiving data as query we need to convert
+        them to list with single data inside.
+        """
         bits = urllib.parse.urlparse(self.path)
         print(self.command, bits)
         buff = bits.query.split('&')
@@ -18,6 +23,11 @@ class SimpleHTTPRequestHandler1(http.server.SimpleHTTPRequestHandler):
 
 def run(server_class=http.server.HTTPServer,
         handler_class=SimpleHTTPRequestHandler1):
+    """
+    Function running server forever on port 8000
+    :param server_class: server
+    :param handler_class: specific type of server
+    """
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
